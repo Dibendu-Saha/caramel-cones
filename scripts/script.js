08d5078b8918f77d04f0b1c42c022d271da04bcf
dibs03 <65119411+dibs03@users.noreply.github.com>
2021-10-16 16:05:11 +0530

    Intersection observer for sticky nav

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,20 @@
 
-var navEl = document.querySelector('.nav-bar');
+var navEl = document.querySelector('.nav-bar'),
+    landingEl = document.querySelector('header'),
+    options = {
+        root: null,
+        threshold: 0
+    };
 
-document.addEventListener('scroll', () => {
-    if (window.scrollY > 753)
+var obs = new IntersectionObserver(entries => {
+    var entry = entries[0];
+
+    if (!entry.isIntersecting)
         navEl.classList.add('nav-bg');
     else
         navEl.classList.remove('nav-bg');
-});
+}, options);
+
+obs.observe(landingEl);
+
+
